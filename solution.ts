fee0bca35013a2b4c46d47e2b4fed1e02c668149
SajidSojib <sajidahmed2401@gmail.com>
2025-11-18 00:59:46 +0600

    problem 7 solved

--- solution.ts
+++ solution.ts
@@ -11,7 +11,7 @@ const formatValue = <T extends number|string|boolean>(value: T):T =>{
 }
 
 
-
+//----------------- 2 -------------------
 const getLength = (value: string | any[]): number => {
     if(typeof value === 'string'){
         return value.length;
@@ -22,6 +22,7 @@ const getLength = (value: string | any[]): number => {
 }
 
 
+//----------------- 3 -------------------
 class Person {
     constructor(public name: string, public age: number){}
     getDetails(){
@@ -31,12 +32,13 @@ class Person {
 
 
 
+//----------------- 4 -------------------
 type Item = {
   title: string;
   rating: number;
 };
 const filterByRating = (items: Item[] ): Item[] =>{
-    const filtered = items.filter((item) => {
+    const filtered:Item[] = items.filter((item:Item):boolean => {
         if(item.rating<0 || item.rating>5){
             throw new Error('Rating must be between 0 and 5');
         }
@@ -47,6 +49,7 @@ const filterByRating = (items: Item[] ): Item[] =>{
 
 
 
+//----------------- 5 -------------------
 type User = {
     id: number;
     name: string;
@@ -59,6 +62,7 @@ const filterActiveUsers = (users: User[]): User[] => {
 
 
 
+//----------------- 6 -------------------
 interface Book {
   title: string;
   author: string;
@@ -69,6 +73,38 @@ const printBookDetails = (book: Book): void => {
     console.log(
       `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable?'Yes':'No'}`
     );
+}
+
+
+
+//----------------- 7 -------------------
+type ValueType = string | number;
+const getUniqueValues = (array1: ValueType[], array2: ValueType[]): ValueType[] => {
+    const result: ValueType[] = [];
+    let sizeOfResult: number = 0;
+
+    const checkUnique = (value: ValueType): boolean => {
+        for (let i = 0; i < sizeOfResult; i++) {
+            if (result[i] === value) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    for (let i = 0; i < array1.length; i++) {
+        if (checkUnique(array1[i])) {
+            result[sizeOfResult] = array1[i];
+            sizeOfResult++;
+        }
+    }
+    for (let i = 0; i < array2.length; i++) {
+        if (checkUnique(array2[i])) {
+            result[sizeOfResult] = array2[i];
+            sizeOfResult++;
+        }
+    }
+    return result;
 }
 
 
